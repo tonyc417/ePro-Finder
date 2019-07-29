bf4209c6f08061665515a0423c6d44aa5255d83d
unknown <tonycruz417@gmail.com>
2019-07-28 23:16:58 -0500

    working on front end

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,8 +3,12 @@ import React from 'react';
 const frontNav = () => {
     return (
         <div className="hero-image">
-            <h4>eRecruit</h4>
-            <h4>Login or Sign Up</h4>
+            <nav className="logoBar">
+                <ul className="nav-links">
+                    <li className="float-left"><a>eRecruit</a></li>
+                    <li className="float-right"><a>Login or Sign Up</a></li>
+                </ul>
+            </nav>
             <nav className="nav-bar">
                 <ul className="nav-links">
                     <li><a>Home</a></li>
