66ddeb88124a5e119b4a174ec6bc9dfbf2629182
unknown <tonycruz417@gmail.com>
2019-07-28 21:41:12 -0500

    working on front page

---
+++ client/src/components/FrontDisplay.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const displayNew = () => {
+    return(
+        <div className="hero-image">
+            <div className="hero-text">
+                <h1>E-Sports</h1>
+                <p>Welcome</p>
+            </div>
+        </div>
+    )
+}
+
+export default displayNew;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const frontNav = () => {
+    return (
+        <div className="hero-image">
+            <h4>eRecruit</h4>
+            <h4>Login or Sign Up</h4>
+            <nav className="nav-bar">
+                <ul className="nav-links">
+                    <li><a>Home</a></li>
+                    <li><a>New Members</a></li>
+                    <li><a>Games</a></li>
+                    <li><a>Find Clan</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default frontNav;
